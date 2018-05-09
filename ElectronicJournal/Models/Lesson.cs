e7b13fd2ef7117227c2296bd77a904954e8af046
Dima Kapitula <kapitula_ak16@nuwm.edu.ua>
2018-05-09 10:21:51 +0300

    Fix the big bug (crashed in AddMissings)

--- ElectronicJournal/Models/Lesson.cs
+++ ElectronicJournal/Models/Lesson.cs
@@ -44,7 +44,6 @@ namespace ElectronicJournal.Models
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
-        [Required]
         [StringLength(10)]
         public string Classroom { get; set; }
 
