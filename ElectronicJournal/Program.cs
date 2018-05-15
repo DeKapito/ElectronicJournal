1bd43437c59b0c1ed8cec08686932ab3ce154238
Dima Kapitula <kapitula_ak16@nuwm.edu.ua>
2018-05-15 19:27:07 +0300

    Added roles

--- ElectronicJournal/Program.cs
+++ ElectronicJournal/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectronicJournal.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ElectronicJournal
@@ -14,7 +17,28 @@ namespace ElectronicJournal
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            var host = BuildWebHost(args); //Next code for first intialization DB
+
+            using (var scope = host.Services.CreateScope())
+            {
+                Task t;
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    t = StartInitializer.InitializeAsync(userManager, rolesManager);
+                    t.Wait(); //!!!!!!!!!!!!
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+        }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
---
+++ ElectronicJournal/StartInitializer.cs
@@ -0,0 +1,52 @@
+﻿using ElectronicJournal.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicJournal
+{
+    public class StartInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "kapitula_ak16@nuwm.edu.ua";
+            string password = "!Journal123";
+            string adminName = "Дмитро";
+            string adminLastName = "Капітула";
+            string adminGroup = "КНІТ-21";
+
+            if (await roleManager.FindByNameAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            if (await roleManager.FindByNameAsync("GroupLeader") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("GroupLeader"));
+            }
+
+            if (await roleManager.FindByNameAsync("Student") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Student"));
+            }
+
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User {
+                    Email = adminEmail,
+                    UserName = adminEmail,
+                    Name = adminName,
+                    LastName = adminLastName,
+                    Group = adminGroup
+                };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
